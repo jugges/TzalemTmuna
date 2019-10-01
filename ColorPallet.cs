6dc7dc079c6cb7c47c31127be7a32ac3fccfd91f
Yuval <yuvalgal51@gmail.com>
2019-10-02 01:29:47 +0300

    design stuff

---
+++ ColorPallet.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TzalemTmuna
+{
+    public struct ColorPallet
+    {
+        public static Color DarkNight
+        {
+            get
+            {
+                return Color.FromArgb(93, 115, 126);
+            }
+        }
+        public static Color LightCream
+        {
+            get
+            {
+                return Color.FromArgb(240, 247, 238);
+            }
+        }
+        public static Color CoralGreen
+        {
+            get
+            {
+                return Color.FromArgb(135, 187, 162);
+            }
+        }
+        public static Color PaleBlue
+        {
+            get
+            {
+                return Color.FromArgb(168, 204, 201);
+            }
+        }
+        public static Color LivingBlue
+        {
+            get
+            {
+                return Color.FromArgb(117, 185, 190);
+            }
+        }
+        public static Color DarkBack
+        {
+            get
+            {
+                return Color.FromArgb(84, 84, 84);
+            }
+        }
+    }
+}
