252b44030cb1cdeedfcc00b519e2bd512d468995
Yuval <yuvalgal51@gmail.com>
2020-05-05 18:22:30 +0300

    added ReportViewer and stuff

---
+++ TzalemTmuna/Forms/PrintableReports.Designer.cs
@@ -0,0 +1,66 @@
+﻿
+namespace TzalemTmuna.Forms
+{
+    partial class PrintableReports
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.reportViewer1 = new Microsoft.Reporting.WinForms.ReportViewer();
+            this.SuspendLayout();
+            // 
+            // reportViewer1
+            // 
+            this.reportViewer1.LocalReport.ReportEmbeddedResource = "TzalemTmuna.Report1.rdlc";
+            this.reportViewer1.Location = new System.Drawing.Point(23, 63);
+            this.reportViewer1.Name = "reportViewer1";
+            this.reportViewer1.ServerReport.BearerToken = null;
+            this.reportViewer1.Size = new System.Drawing.Size(789, 832);
+            this.reportViewer1.TabIndex = 0;
+            // 
+            // PrintableReports
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(835, 918);
+            this.Controls.Add(this.reportViewer1);
+            this.Icon = global::TzalemTmuna.Properties.Resources.tico;
+            this.MaximizeBox = false;
+            this.Name = "PrintableReports";
+            this.ShadowType = MetroFramework.Forms.MetroFormShadowType.AeroShadow;
+            this.Style = MetroFramework.MetroColorStyle.Default;
+            this.Text = "Printable Reports";
+            this.Theme = MetroFramework.MetroThemeStyle.Default;
+            this.Load += new System.EventHandler(this.PrintableReports_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Microsoft.Reporting.WinForms.ReportViewer reportViewer1;
+    }
+}
---
+++ TzalemTmuna/Forms/PrintableReports.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using TzalemTmuna.DB;
+using TzalemTmuna.Utilities;
+using TzalemTmuna.Entities;
+using TzalemTmuna.Data;
+using TzalemTmuna.Statics;
+
+namespace TzalemTmuna.Forms
+{
+    public partial class PrintableReports : MetroFramework.Forms.MetroForm
+    {
+        public PrintableReports()
+        {
+            InitializeComponent();
+            StyleManager = new MetroFramework.Components.MetroStyleManager
+            {
+                Owner = this,
+                Theme = Statics.Theme.metroThemeStyle,
+                Style = Statics.Theme.metroColorStyle
+            };
+        }
+
+        private void PrintableReports_Load(object sender, EventArgs e)
+        {
+
+            this.reportViewer1.RefreshReport();
+        }
+    }
+}
